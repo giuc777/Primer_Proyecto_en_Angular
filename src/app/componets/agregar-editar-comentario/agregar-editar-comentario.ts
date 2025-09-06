5998648996b31532f4f5c582fe584ea19bd2962a
Gady <gadyus18@gmail.com>
2025-09-06 17:25:14 -0600

    Se agrego agregar comentario para funcionar

--- src/app/componets/agregar-editar-comentario/agregar-editar-comentario.ts
+++ src/app/componets/agregar-editar-comentario/agregar-editar-comentario.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { FormGroup,FormBuilder,ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { Comentario } from '../../services/comentario';
+
 
 @Component({
   selector: 'app-agregar-editar-comentario',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink ],
   templateUrl: './agregar-editar-comentario.html',
   styleUrl: './agregar-editar-comentario.scss'
 })
 export class AgregarEditarComentario {
+  agregarComentario: FormGroup;
 
+  constructor(private readonly fb:FormBuilder){
+    this.agregarComentario = this.fb.group({
+      titulo:['', Validators.required],
+      creador:['', Validators.required],
+      texto:['', Validators.required]
+    });
+  }
+
+  agregar(){
+    console.log(this.agregarComentario.value);
+    const nuevoComentario: Comentario ={
+      titulo: this.agregarComentario.value?.titulo,
+      creador: this.agregarComentario.value?.creador,
+      texto: this.agregarComentario.value?.texto,
+      fechaCreacion: new Date()
+    };
+    console.log(nuevoComentario);
+  }
 }
